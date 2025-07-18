3fbbc0a455ff7aebfeedac962e254f137256167f
vedanshjainvj <vedansh.code@gmail.com>
2025-07-18 16:38:19 +0530

    CORS Config

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,41 +1,31 @@
 // -------------------- PACKAGE IMPORT FILES -------------------- //
 import dotenv from "dotenv"
+dotenv.config();
 
 // ----------------------------------- Initilization of .env file -------------------------------------------
-const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://task3wsolutions.onrender.com"
-];
-
 export const corsOptions = {
     origin: function (origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://internship-task-three-w.vercel.app"
+        ];
         if (!origin || allowedOrigins.includes(origin)) {
-            console.log(origin)
+            console.log("Allowed Origin: ", origin);
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS"));
         }
     },
-    "Access-Control-Allow-Origin": "*",
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'withCredentials']
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
-
-dotenv.config();
 
 export const envProvider = {
     PORT: process.env.PORT,
     DB_URI: process.env.DB_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.AWS_REGION,
-    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-    EMAIL: process.env.EMAIL,
-    PASSWORD: process.env.PASSWORD,
     NODE_ENV: process.env.NODE_ENV
 }
 
